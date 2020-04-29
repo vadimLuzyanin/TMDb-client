7b9ed32754e783fbf318469da2ee4a65a16674f1
vadimLuzyanin <luzianin999@inbox.ru>
2020-04-29 11:16:36 +0300

    Updates

---
+++ precache-manifest.af646167deb3676111f0f93e24b2207d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99577108b5b48af225a0c6fb020b89e",
+    "url": "/TMDb-client/index.html"
+  },
+  {
+    "revision": "6ce34737bc77fdf41079",
+    "url": "/TMDb-client/static/js/2.65dcc7a7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TMDb-client/static/js/2.65dcc7a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46ae0eb3b7c7beffe51c",
+    "url": "/TMDb-client/static/js/main.fccbd864.chunk.js"
+  },
+  {
+    "revision": "03c2863e89c6ef6f7b40",
+    "url": "/TMDb-client/static/js/runtime-main.8bbd3625.js"
+  }
+]);
